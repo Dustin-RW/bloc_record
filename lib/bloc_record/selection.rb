1313f8bb9aadd163b550ea4fef3e520488097dc8
Dustin <dustinwaggoner@comcast.net>
2017-02-28 20:23:00 -0800

    fixed #join bug

--- lib/bloc_record/selection.rb
+++ lib/bloc_record/selection.rb
@@ -231,24 +231,26 @@ module Selection
       rows = connection.execute <<-SQL
         SELECT * FROM #{table} #{joins}
       SQL
+    else
       case args.first
-      when String
-        rows = connection.execute <<-SQL
-          SELECT * FROM #{table} #{BlocRecord::Utility.sql_strings(args.first)};
-        SQL
-      when Symbol
-        rows = connection.execute <<-SQL
-          SELECT * FROM #{table}
-          INNER JOIN #{args.first} ON #{args.first}.#{table}_id = #{table}.id;
-        SQL
-      when Hash
-        key = args.first.keys[0]
-        value = args.first.values[0]
-        rows = connection.execute <<-SQL
-          SELECT * FROM #{table}
-          INNER JOIN #{key} ON #{key}.#{table}_id = #{table}.id
-          INNER JOIN #{value} ON #{value}.#{key}_id = #{key}.id
-        SQL
+        when String
+          rows = connection.execute <<-SQL
+            SELECT * FROM #{table} #{BlocRecord::Utility.sql_strings(args.first)};
+          SQL
+        when Symbol
+          rows = connection.execute <<-SQL
+            SELECT * FROM #{table}
+            INNER JOIN #{args.first} ON #{args.first}.#{table}_id = #{table}.id;
+          SQL
+        when Hash
+          key = args.first.keys[0]
+          value = args.first.values[0]
+          rows = connection.execute <<-SQL
+            SELECT * FROM #{table}
+            INNER JOIN #{key} ON #{key}.#{table}_id = #{table}.id
+            INNER JOIN #{value} ON #{value}.#{key}_id = #{key}.id
+          SQL
+        end
       end
     end
 
