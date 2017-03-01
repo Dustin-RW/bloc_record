4108d2bc3d513be3a032abc1eeddf2ca85bce35d
Dustin <dustinwaggoner@comcast.net>
2017-03-01 15:31:55 -0800

    added #update functionality

--- lib/bloc_record/selection.rb
+++ lib/bloc_record/selection.rb
@@ -268,7 +268,9 @@ module Selection
 
   # Wraps each row in a record object
   def rows_to_array(rows)
-    rows.map { |row| new(Hash[columns.zip(row)]) }
+    collection = BlocRecord::Collection.new
+    rows.each { |row| collection << new(Hash[columns.zip(row)]) }
+    collection
   end
 
   #string provided includes a ASC or DESC.  If not, provided default ASC for SQL
